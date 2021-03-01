c02e98e694c8eb51919d348b38cadad259a8eab0
Jacques <jack.polart@gmail.com>
2021-03-01 14:03:02 +0100

    First commit

---
+++ src/components/Today.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Today extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date(Date.now())}
+  }
+
+  render() {
+    return(
+      <h1>hello world : {this.state.date.toLocaleDateString()}</h1>
+    );
+  }
+}
+
+export default Today;
